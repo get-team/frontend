55b73582438e1055677186f6e47d85584f2b41a1
km6293 <69450660+km6293@users.noreply.github.com>
2023-11-15 21:38:24 +0900

    feat: complete login Page design

--- screens/Login/Login.js
+++ screens/Login/Login.js
@@ -1,58 +1,128 @@
 import React, { Fragment } from "react";
-import { View, Text, StyleSheet, TouchableOpacity, SafeAreaView, StatusBar, TextInput } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StatusBar,
+  TextInput,
+  TouchableWithoutFeedback,
+  Keyboard,
+  TouchableOpacity
+} from "react-native";
 import { useNavigation } from "@react-navigation/native";
-import Ellipse from "../../component/Svg/Ellipse"
-import ArrowLeft from "../../component/Svg/ArrowLeft"
-import AhevronDown from "../../component/Svg/AhevronDown"
-import styles from "./LoginStyles"
+import DropDown from "../../component/DropDown/DropDown";
+import Ellipse from "../../component/Svg/Ellipse";
+import ArrowLeft from "../../component/Svg/ArrowLeft";
+import Kakao from "../../component/Svg/Kakao";
+import Naver from "../../component/Svg/Naver";
+import Google from "../../component/Svg/Google";
+import Apple from "../../component/Svg/Apple";
+import styles from "./LoginStyles.js";
 
 const Login = () => {
-
+  
   const navigation = useNavigation();
-
   return (
     <Fragment>
       <SafeAreaView style={styles.container}>
-        <View style={styles.containerArea}>      
-          {/* 최상단 */}
-          <StatusBar style='auto'/>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={styles.containerArea}>      
+            {/* 최상단 */}
+            <StatusBar style='auto'/>
 
-          {/* statusbar */}
-          <View style={styles.statusbar}>
-          </View>
-
-          {/* 헤더 부분 */}
-          <View style={styles.header}>
-            <View style={styles.button}> 
-              <Ellipse />
-              <ArrowLeft/>
+            {/* statusbar */}
+            <View style={styles.statusbar}>
             </View>
-          </View>
 
-          {/* 로그인 폼 */}
-          <View style={styles.loginFormContainer}>
-            <View style={styles.loginTitle}>
-              <View style={styles.spacing} />
-              <Text style={styles.loginTitleText}>로그인</Text>
-              <View style={styles.loginTitleContentView}>
-                <Text style={styles.loginTitleContent}>
-                  계정에 로그인하려면 귀하의 휴대폰 번호를 입력하세요.
+            {/* 헤더 부분 */}
+            <View style={styles.header}>
+              <TouchableOpacity style={styles.button}> 
+                <Ellipse />
+                <ArrowLeft/>
+              </TouchableOpacity>
+            </View>
+
+            {/* 로그인 폼 */}
+            <View style={styles.loginFormContainer}>
+              <View style={styles.loginTitle}>
+                <View style={styles.spacing} />
+                <Text style={styles.loginTitleText}>로그인</Text>
+                <View style={styles.loginTitleContentView}>
+                  <Text style={styles.loginTitleContent}>
+                    계정에 로그인하려면 귀하의 휴대폰 번호를 입력하세요.
+                  </Text>
+                </View>
+              </View>
+
+              <View style={styles.loginContent}>
+                <View style={styles.phonePrefix}>
+                  <DropDown/>
+                </View>
+                  <View style={styles.phoneNumber}>
+                    <TextInput 
+                      keyboardType="number-pad"
+                      maxLength={8}
+                    />
+                  </View>
+              </View>
+
+              <View style={styles.gubun}>
+                <View style={styles.line} />
+                <Text style={styles.lineText}>또는</Text>
+                <View style={styles.line} />
+              </View>
+
+              <View style={styles.socialContent}>
+                <TouchableOpacity style={styles.kakaoButton}>
+                  <Kakao />
+                  <Text style={styles.kakaoText}>카카오로 계속하기</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.naverButton}>
+                  <Naver />
+                  <Text style={styles.naverText}>네이버로 계속하기</Text>
+                </TouchableOpacity>
+              </View>
+              
+              <View style={styles.totalButton}>
+                <TouchableOpacity style={styles.halfButton}>
+                  <Google/>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.halfButton}>
+                  <Apple/>
+                </TouchableOpacity>
+              </View>
+
+            </View>
+
+            {/* 로그인 버튼 */}
+            <View style={styles.footer}>
+              <View style={styles.footerLine} />
+              <TouchableOpacity
+                onPress={() => {
+                  navigation.removeListener
+                  navigation.navigate("Login", { screen: 'Login' })
+                }}
+                style={styles.loginButton}
+              >
+                <Text style={styles.loginButtonText}>로그인</Text>
+              </TouchableOpacity>
+              <View style={styles.footerText}>
+                <Text>
+                  계정이 없으신가요?
                 </Text>
+                <TouchableOpacity>
+                  <Text style={styles.signInText}>
+                    에코히어로 가입하기
+                  </Text>
+                </TouchableOpacity>
               </View>
             </View>
-
-            <View style={styles.loginNumberContainer}>
-              
-            </View>
+            
           </View>
-
-          {/* 로그인 버튼 */}
-          <View style={styles.loginButtonContainer}>
-
-          </View>
-          
-        </View>
+        </TouchableWithoutFeedback>
       </SafeAreaView>
+
     </Fragment>
   )
 }
--- screens/Login/LoginStyles.js
+++ screens/Login/LoginStyles.js
@@ -25,7 +25,6 @@ const styles = StyleSheet.create({
     "flexShrink": 0
   },
   header: {
-    "backgroundColor": "yellow",
     "width": 390,
     "height": 74,
     "paddingTop": 16,
@@ -42,7 +41,6 @@ const styles = StyleSheet.create({
     "flexShrink": 0
   },
   loginFormContainer:{
-    "backgroundColor": "red",
     "display": "inline-flex",
     "paddingTop": 0,
     "paddingRight": 16,
@@ -83,17 +81,193 @@ const styles = StyleSheet.create({
     "fontWeight": "400",
     "lineHeight": 25.6
   },
-  loginNumberContainer:{
-    "backgroundColor": "yellow",
+  loginContent:{
     width: 360,
     height: 56,
     gap: 16,
     "display": "flex",
-    "flexDirection": "column",
+    "flexDirection": "row",
     "alignItems": "flex-start",
-    "alignSelf": "stretch"
+    "alignSelf": "stretch",
+    zIndex:10
   },
-  
+  phonePrefix:{
+    width: 108,
+    height: 56,
+    gap: 6,
+  },
+  phoneNumber:{
+    "backgroundColor": "rgb(255, 255, 255)",
+    width:236,
+    height:50,
+    gap: 6,
+    "borderTopLeftRadius": 8,
+    "borderTopRightRadius": 8,
+    "borderBottomRightRadius": 8,
+    "borderBottomLeftRadius": 8,
+    "borderTopWidth": 1,
+    "borderRightWidth": 1,
+    "borderBottomWidth": 1,
+    "borderLeftWidth": 1,
+    "paddingTop": 3,
+    "paddingRight": 10,
+    "paddingBottom": 3,
+    "paddingLeft": 10,
+    "justifyContent": "center",
+  },
+  gubun:{
+    paddingTop: 36,
+    paddingBottom: 24,
+    width:360,
+    height:22,
+    gap:12,
+    flexDirection:'row',
+    "justifyContent": "center",
+    "alignItems": "center",
+  },
+  line:{
+    width:155,
+    "borderWidth": 1,
+    "borderColor": "#2222224D",
+    "borderStyle": "solid",
+  },
+  lineText:{
+    width:26,
+    height:15,
+  },
+  socialContent:{
+    "alignSelf": "stretch",
+    gap: 16,
+    paddingBottom: 16
+  },
+  kakaoButton:{
+    "display": "flex",
+    "paddingTop": 14,
+    "paddingRight": 0,
+    "paddingBottom": 14,
+    "paddingLeft": 0,
+    "justifyContent": "center",
+    "alignItems": "center",
+    "alignSelf": "stretch",
+    "borderTopLeftRadius": 100,
+    "borderTopRightRadius": 100,
+    "borderBottomRightRadius": 100,
+    "borderBottomLeftRadius": 100,
+    "backgroundColor": "#FEE500",
+    "flexDirection": "row",
+    width: 360,
+    height: 46,
+  },
+  kakaoText:{
+    width: 115,
+    height: 24,
+    fontWeight: 700,
+    fontSize: 16,
+    lineHeight: 24
+  },
+  naverButton:{
+    "display": "flex",
+    "paddingTop": 14,
+    "paddingRight": 0,
+    "paddingBottom": 14,
+    "paddingLeft": 0,
+    "justifyContent": "center",
+    "alignItems": "center",
+    "alignSelf": "stretch",
+    "borderTopLeftRadius": 100,
+    "borderTopRightRadius": 100,
+    "borderBottomRightRadius": 100,
+    "borderBottomLeftRadius": 100,
+    "backgroundColor": "#1EC800",
+    "flexDirection": "row",
+    width: 360,
+    height: 46,
+  },
+  naverText:{
+    width: 115,
+    height: 24,
+    fontWeight: 700,
+    fontSize: 16,
+    lineHeight: 24,
+    color: '#FFFFFF'
+  },
+  totalButton:{
+    flexDirection:'row',
+    gap: 16
+  },
+  halfButton:{
+    "display": "flex",
+    "paddingTop": 14,
+    "paddingRight": 0,
+    "paddingBottom": 14,
+    "paddingLeft": 0,
+    "justifyContent": "center",
+    "alignItems": "center",
+    "alignSelf": "stretch",
+    "borderTopLeftRadius": 100,
+    "borderTopRightRadius": 100,
+    "borderBottomRightRadius": 100,
+    "borderBottomLeftRadius": 100,
+    "borderWidth": 1,
+    "borderColor": "#2222224D",
+    "borderStyle": "solid",
+    "flexDirection": "row",
+    width: 172,
+    height: 46,
+  },
+  footer: {
+    // backgroundColor:'blue',
+    height: 126,
+    marginTop:150,
+    "justifyContent": "center",
+    "alignItems": "center",
+  },
+  loginButton: {
+    "display": "flex",
+    "paddingTop": 14,
+    "paddingRight": 0,
+    "paddingBottom": 14,
+    "paddingLeft": 0,
+    "justifyContent": "center",
+    "alignItems": "center",
+    "borderTopLeftRadius": 100,
+    "borderTopRightRadius": 100,
+    "borderBottomRightRadius": 100,
+    "borderBottomLeftRadius": 100,
+    "backgroundColor": "#FF5D00",
+    height:52,
+    width:358
+  },
+  loginButtonText: {
+    "color": "#FFF",
+    "fontSize": 16,
+    "fontStyle": "normal",
+    "fontWeight": "700",
+    "lineHeight": 24
+  },
+  footerText:{
+    width: '100%',
+    height: 28,
+    paddingTop: 6,
+    textAlign:'center',
+    flexDirection: 'row', // 가로로 나열하도록 설정
+    justifyContent: 'center', // 수평 중앙 정렬
+    alignItems: 'center', // 수직 중앙 정렬
+  },
+  footerLine:{
+    marginBottom: 14,
+    height:1,
+    width:'100%',
+    "borderWidth": 1,
+    "borderColor": "#2222224D",
+    "borderStyle": "solid",
+  },
+  signInText:{
+    fontWeight: 700,
+    color: '#FF5D00',
+    textAlign: 'center',
+    marginLeft: 5
+  }
 })
 
 export default styles;
