a9ab9bfe6efaf2f7de10f903842991d90992ddc0
km6293 <69450660+km6293@users.noreply.github.com>
2023-11-15 09:49:20 +0900

    feat: add svg

---
+++ component/Svg/Kakao.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Svg, { Path } from 'react-native-svg';
+
+const Kakao = () => {
+  return (
+    <Svg width="31" height="20" viewBox="0 0 31 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <Path fillRule="evenodd" clipRule="evenodd" d="M0.750488 8.4129C0.750488 11.03 2.43832 13.3375 5.00907 14.7104L3.9279 18.7092C3.83257 19.0625 4.23124 19.3438 4.53782 19.1388L9.27956 15.9721C9.68036 16.0113 10.0867 16.0342 10.5005 16.0342C15.8847 16.0342 20.2505 12.6221 20.2505 8.4129C20.2505 4.20362 15.8847 0.791504 10.5005 0.791504C5.11524 0.791504 0.750488 4.20362 0.750488 8.4129Z" fill="black"/>
+    </Svg>
+  );
+}
+
+export default Kakao;
---
+++ component/Svg/Naver.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Svg, { Path } from 'react-native-svg';
+
+const Naver = () => {
+  return (
+    <Svg width="37" height="26" viewBox="0 0 37 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <Path d="M26.5 13C26.5 5.8203 20.6797 0 13.5 0C6.3203 0 0.5 5.8203 0.5 13C0.5 20.1797 6.3203 26 13.5 26C20.6797 26 26.5 20.1797 26.5 13Z" fill="#1EC800"/>
+      <Path fillRule="evenodd" clipRule="evenodd" d="M17.1784 4V13.2434L10.8447 4H4V22.3236H10.8226V13.079L17.1553 22.3236H24V4H17.1784Z" fill="white"/>
+    </Svg>
+  );
+}
+
+export default Naver;
